6387f0d4a0f80de9c7636401196b343306446c6e
Cl4r1ty-1 <bentomo2009@outlook.com>
2025-04-18 12:51:41 +0800

    idek man

--- Assignments/Project 1 - UNO/src/app.py
+++ Assignments/Project 1 - UNO/src/app.py
@@ -1,4 +1,4 @@
-from deck import deck
+from deck import deck, colours
 from players import get_play_order
 import random
 import sys
@@ -51,10 +51,80 @@ def authenticate(filename) -> None:
     play_game()
 
 def play_game():
-    play_order = get_play_order()
-    played_card = deck[-1]
-    deck.pop()
+    global PLAY_ORDER
+    global turn_order
+    global play_colour
+    PLAY_ORDER = get_play_order()
+    turn_order = PLAY_ORDER
+    played_card = deck[0]
+    deck.pop(0)
+    print(f'A {played_card} card was played')
+    while played_card.type == "Wild":
+        print("Can't start on a wild card!")
+        deck.append(played_card)
+        played_card = deck[0]
+        deck.pop(0)
+    play_colour = played_card.colour
+    check_card(played_card)
 
+def init_turn(played_card):
+    global turn_order
+    global play_colour
+    if len(turn_order) == 0:
+        print("Game Over!")
+        end_game()
+        return
+    current_player = turn_order[0]
+    print(f"It's {current_player.name}'s turn!")
+    print(f"Current colour: {play_colour}")
+    print(f"Current card: {played_card}")
+    if current_player.is_cpu:
+        chosen_card = random.choice(current_player.deck)
+        print(f"{current_player.name} played {chosen_card}")
+        current_player.deck.remove(chosen_card)
+    else:
+        chosen_card = input("Enter the card you want to play (or 'Draw' a card): ").title()
+        while chosen_card not in current_player.deck:
+            print("Invalid card! Try again.")
+            played_card = input("Enter the card you want to play: ")
+        while chosen_card not (played_card.type in chosen_card or played_card.colour in chosen_card): # fix this
+            print("Invalid card! Try again.")
+            chosen_card = input("Enter the card you want to play: ").title()
+        current_player.deck.remove(played_card)
+    
+    check_card(played_card)
+
+
+def check_card(played_card):
+    global play_colour
+    global turn_order
+    if played_card.colour == "Wild":
+        if turn_order[0].is_cpu:
+            play_colour = random.choice(colours)
+        else:
+            colour_choice = input("Choose colour (Blue, Green, Yellow or Red): ").title()
+            while colour_choice not in colours:
+                print("Invalid Colour!")
+                colour_choice = input("Choose colour (Blue, Green, Yellow or Red): ").title()
+            play_colour = colour_choice
+
+        if played_card.type == "Draw Four":
+            for _ in range(4):
+                turn_order[1].deck.append(deck[0])
+                deck.pop(0)
+            turn_order.pop(1)
+    if played_card.type == "Draw Two":
+        for _ in range(2):
+            turn_order[1].deck.append(deck[0])
+            deck.pop(0)
+        turn_order.pop(1)
+    if played_card.type == "Skip":
+        turn_order.pop(1)
+    if played_card.type == "Reverse":
+        turn_order.reverse()
+
+def end_game():
+    pass
 
 def get_scores():
     pass
--- Assignments/Project 1 - UNO/src/players.py
+++ Assignments/Project 1 - UNO/src/players.py
@@ -2,27 +2,26 @@ from deck import deck
 import random
 
 class Player:
-    def __init__(self, name: str, score: int, deck: list):
+    def __init__(self, name: str, is_cpu: bool, score: int, games_won: int, deck: list):
         self.name = name
+        self.is_cpu = is_cpu
         self.score = score
+        self.games_won = games_won
         self.deck = deck
     def __repr__(self):
         return self.name
     
-class CPU(Player):
-    pass
-
 def get_player_deck() -> list:
     player_deck = []
-    for i in range(7):
-        player_deck.append(deck[-1])
-        deck.pop()
+    for _ in range(7):
+        player_deck.append(deck[0])
+        deck.pop(0)
     return player_deck
 
 def get_players() -> list:
     player = input("Enter your player name: ")   
     cpus = int(input("Enter the number of computer generated players you would like to play against: "))
-    players = [Player(player, 0, get_player_deck())] + [CPU(f'CPU{i+1}', 0, get_player_deck()) for i in range(cpus)]
+    players = [Player(player, False, 0, 0, get_player_deck())] + [Player(f'CPU{i+1}', True, 0, 0, get_player_deck()) for i in range(cpus)]
     return players
 
 def get_play_order() -> list:
