08a9a8a3a9b235a32ec61e0a99c139f0940716d7
Cl4r1ty-1 <bentomo2009@outlook.com>
2025-04-19 14:56:42 +0800

    oh my FUCKING god man i really didnt wanna do this but what the hell

--- Assignments/Project 1 - UNO/src/app.py
+++ Assignments/Project 1 - UNO/src/app.py
@@ -1,4 +1,4 @@
-from deck import deck, colours
+from deck import deck, COLOURS, UnoCard
 from players import get_play_order
 import random
 import sys
@@ -42,86 +42,176 @@ def authenticate(filename) -> None:
                 filename = 'users.txt'
 
 
-    username = input("Enter your name to login: ")
+    username = input("Enter your name to login: ").title()
     while username not in user_db:
         print("You are not authorized to play this game. Try again.")
-        username = input("Enter your name to login: ")
+        username = input("Enter your name to login: ").title()
     print("Authenticated!")
     print()
     play_game()
 
 def play_game():
-    global PLAY_ORDER
+    global play_order
     global turn_order
     global play_colour
-    PLAY_ORDER = get_play_order()
-    turn_order = PLAY_ORDER
+    global played_card
+    play_order = get_play_order()
+    turn_order = play_order[:]
+    print("\nGame is starting!\n")
+
     played_card = deck[0]
     deck.pop(0)
-    print(f'A {played_card} card was played')
+    print(f'A {played_card} was played to start')
     while played_card.type == "Wild":
         print("Can't start on a wild card!")
         deck.append(played_card)
         played_card = deck[0]
         deck.pop(0)
+        print(f'A {played_card} was played to start')
     play_colour = played_card.colour
-    check_card(played_card)
-
-def init_turn(played_card):
-    global turn_order
-    global play_colour
-    if len(turn_order) == 0:
-        print("Game Over!")
-        end_game()
-        return
-    current_player = turn_order[0]
-    print(f"It's {current_player.name}'s turn!")
-    print(f"Current colour: {play_colour}")
-    print(f"Current card: {played_card}")
-    if current_player.is_cpu:
-        chosen_card = random.choice(current_player.deck)
-        print(f"{current_player.name} played {chosen_card}")
-        current_player.deck.remove(chosen_card)
-    else:
-        chosen_card = input("Enter the card you want to play (or 'Draw' a card): ").title()
-        while chosen_card not in current_player.deck:
-            print("Invalid card! Try again.")
-            played_card = input("Enter the card you want to play: ")
-        while chosen_card not (played_card.type in chosen_card or played_card.colour in chosen_card): # fix this
-            print("Invalid card! Try again.")
-            chosen_card = input("Enter the card you want to play: ").title()
-        current_player.deck.remove(played_card)
+    check_card(played_card, is_start=True)
+    print()
     
-    check_card(played_card)
+    while True:
+        for current_player in turn_order[:]:
+            init_turn(current_player)
 
+            if not current_player.deck:
+                end_game()
 
-def check_card(played_card):
+            if turn_order != play_order:
+                break
+
+        turn_order = play_order[:]
+
+def init_turn(current_player):
+    global played_card
+    global play_colour
+    print(f"\nIt is {current_player.name}'s turn!\n")
+    
+    # CPU turn logic
+    if current_player.is_cpu:
+        # Check if the CPU has a playable card
+        playable_cards = [
+            card for card in current_player.deck
+            if card.colour == play_colour or card.type == played_card.type or card.colour == "Wild"
+        ]
+
+        if playable_cards:
+            chosen_card = random.choice(current_player.deck)
+            print(f"{current_player.name} played a {chosen_card}!")
+            current_player.deck.remove(chosen_card)
+            check_card(chosen_card)
+            played_card = chosen_card
+            play_colour = played_card.colour
+        else:
+            # Draw a card from the deck
+            print(f"{current_player.name} has no playable cards and draws a card.")
+            drawn_card = deck[0]
+            deck.pop(0)
+            current_player.deck.append(drawn_card)
+            print(f"{current_player.name} drew a {drawn_card}.")
+
+            # Check if the drawn card is playable
+            if drawn_card.colour == play_colour or drawn_card.type == played_card.type or drawn_card.colour == "Wild":
+                print(f"{current_player.name} played the drawn card: {drawn_card}!")
+                current_player.deck.remove(drawn_card)
+                check_card(drawn_card)
+                played_card = drawn_card
+                play_colour = played_card.colour
+            else:
+                print(f"{current_player.name} cannot play the drawn card.")
+    
+    # Player turn logic
+    else:
+        str_deck = [str(i) for i in current_player.deck]
+        print(f'Your deck: {str_deck}')
+        print(f'Played card: {played_card}')
+        
+        while True:
+            print("What would you like to do?")
+            print("1. Play a card")
+            print("2. Draw a card from the deck")
+            option = input("> ")
+
+            if option == "1":
+                print("Select the card you would like to play. (Type card name as shown in deck)")
+                chosen_card_str = input("> ")
+                if chosen_card_str in str_deck:
+
+                    chosen_card = next(card for card in current_player.deck if str(card) == chosen_card_str)
+
+                    if chosen_card.colour == play_colour or chosen_card.type == played_card.type:
+                        print(f"You played a {chosen_card}!")
+                        current_player.deck.remove(chosen_card)
+                        check_card(chosen_card)
+                        played_card = chosen_card
+                        play_colour = played_card.colour
+                        break
+                    else:
+                        print("Can't play that card!") 
+                else:
+                    print("You don't have that card!")
+            elif option == "2":
+                # Draw a card from the deck
+                print("You draw a card.")
+                drawn_card = deck[0]
+                deck.pop(0)
+                current_player.deck.append(drawn_card)
+                print(f"{current_player.name} drew a {drawn_card}.")
+
+                # Check if the drawn card is playable
+                if drawn_card.colour == play_colour or drawn_card.type == played_card.type or drawn_card.colour == "Wild":
+                    print(f"{current_player.name} played the drawn card: {drawn_card}!")
+                    current_player.deck.remove(drawn_card)
+                    check_card(drawn_card)
+                    played_card = drawn_card
+                    play_colour = played_card.colour
+                break
+            else:
+                print("Invalid option!")
+
+def check_card(played_card, is_start=False):
     global play_colour
     global turn_order
+    global play_order
     if played_card.colour == "Wild":
         if turn_order[0].is_cpu:
-            play_colour = random.choice(colours)
+            play_colour = random.choice(COLOURS)
         else:
             colour_choice = input("Choose colour (Blue, Green, Yellow or Red): ").title()
-            while colour_choice not in colours:
+            while colour_choice not in COLOURS:
                 print("Invalid Colour!")
                 colour_choice = input("Choose colour (Blue, Green, Yellow or Red): ").title()
             play_colour = colour_choice
+            print(f"Play colour is now {play_colour}!")
 
         if played_card.type == "Draw Four":
             for _ in range(4):
                 turn_order[1].deck.append(deck[0])
                 deck.pop(0)
             turn_order.pop(1)
-    if played_card.type == "Draw Two":
-        for _ in range(2):
-            turn_order[1].deck.append(deck[0])
-            deck.pop(0)
-        turn_order.pop(1)
-    if played_card.type == "Skip":
-        turn_order.pop(1)
-    if played_card.type == "Reverse":
+    elif played_card.type == "Draw Two":
+        if not is_start:
+            for _ in range(2):
+                turn_order[1].deck.append(deck[0])
+                deck.pop(0)
+            turn_order.pop(1)
+            print(f"{turn_order[1].name} drew 2 cards and is skipped!")
+        else:
+            for _ in range(2):
+                turn_order[0].deck.append(deck[0])
+                deck.pop(0)
+            turn_order.pop(0)
+            print(f"{turn_order[0].name} drew 2 cards and is skipped!")
+    elif played_card.type == "Skip":
+        if not is_start:
+            turn_order.pop(1)
+        else:
+            turn_order.pop(0)
+    elif played_card.type == "Reverse":
         turn_order.reverse()
+        play_order.reverse()
 
 def end_game():
     pass
--- Assignments/Project 1 - UNO/src/deck.py
+++ Assignments/Project 1 - UNO/src/deck.py
@@ -6,12 +6,20 @@ class UnoCard:
         self.type = type
     def __repr__(self):
         return f'{self.colour} {self.type}'
+    
+    def __eq__(self, other):
+        if isinstance(other, UnoCard):
+            return self.colour == other.colour and self.type == other.type
+        return False
+
+    def __hash__(self):
+        return hash((self.colour, self.type))
  
-colours = ['Blue', 'Green', 'Red', 'Yellow']
+COLOURS = ['Blue', 'Green', 'Red', 'Yellow']
 types = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Draw Two', 'Reverse']
 types = sorted(types + types)[1:]
 
-deck = [UnoCard(c, t) for c in colours for t in types] + \
+deck = [UnoCard(c, t) for c in COLOURS for t in types] + \
     [UnoCard('Wild', 'Wild') for _ in range (4)] + \
     [UnoCard('Wild', 'Draw Four') for _ in range(4)]
 
