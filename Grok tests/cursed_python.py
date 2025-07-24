486126f8c3b30b1c3565faea5d33e5027a585654
Cl4r1ty-1 <bentomo2009@outlook.com>
2025-07-24 08:49:34 +0800

    idek bruh

--- Grok tests/cursed_python.py
+++ Grok tests/cursed_python.py
@@ -8,3 +8,15 @@ a()
 def c(): (b:="hellO"), print(b)
 
 c()
+
+def d():
+    global b
+    b = False
+    if b:
+        return False
+    else:
+        return (b := "what")
+    
+e = d()
+print(e)
+print(b)
