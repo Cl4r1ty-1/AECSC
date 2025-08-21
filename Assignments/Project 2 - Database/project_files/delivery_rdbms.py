e9ddc067267ac6d6735104664ecbe1812eca2c8a
Cl4r1ty-1 <bentomo2009@outlook.com>
2025-08-21 10:24:33 +0800

    reports and last queries

--- Assignments/Project 2 - Database/project_files/delivery_rdbms.py
+++ Assignments/Project 2 - Database/project_files/delivery_rdbms.py
@@ -1,7 +1,7 @@
 import sqlite3
 import csv
 import tkinter as tk
-from tkinter import messagebox, filedialog, ttk
+from tkinter import messagebox, filedialog, ttk, simpledialog
 
 conn = sqlite3.connect("delivery.db")
 c = conn.cursor()
@@ -10,15 +10,15 @@ def create_tables():
     c.execute("DROP TABLE IF EXISTS Customers")
     c.execute("DROP TABLE IF EXISTS Deliveries")
     c.execute("DROP TABLE IF EXISTS Drivers")
-
+    # to do: set NOT NULL on required values
     c.execute("""
               CREATE TABLE Customers (
-                customer_id INTEGER PRIMARY KEY,
-                first_name TEXT,
+                customer_id INTEGER PRIMARY KEY NOT NULL,
+                first_name TEXT NOT NULL,
                 last_name TEXT,
-                street_address TEXT,
-                suburb TEXT,
-                post_code INTEGER
+                street_address TEXT NOT NULL,
+                suburb TEXT NOT NULL,
+                post_code INTEGER NOT NULL
               )
             """)
     
@@ -76,7 +76,28 @@ def create_query_frame(root, query):
     for widget in root.winfo_children():
         widget.destroy()
 
-    c.execute(query)
+    if query == 'query_5':
+        specific_date = simpledialog.askstring("Specific Date", "Enter date (DD/MM/YYYY): ", initialvalue="01/01/2000")
+        # todo: input handling
+        c.execute("""
+                  SELECT Deliveries.delivery_docket, Deliveries.collected_from, Deliveries.date_collected, Deliveries.weight, Deliveries.deliver_to, Deliveries.date_delivered,
+                  CONCAT(Drivers.first_name, ' ', Drivers.last_name) AS driver,
+                  CONCAT(Customers.first_name, ' ', Customers.last_name) AS customer
+                  FROM Deliveries
+                  INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id
+                  INNER JOIN Customers ON Deliveries.customer_id = Customers.customer_id
+                  WHERE Deliveries.date_delivered = ?
+                  """, (specific_date,))
+    elif query == 'query_6':
+        low_weight = simpledialog.askfloat("Low Weight", "Enter the minimum weight: ")
+        high_weight = simpledialog.askfloat("High Weight", "Enter the maximum weight: ")
+        c.execute("""
+                 SELECT Deliveries.weight, Drivers.* FROM Deliveries 
+                 INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id 
+                 WHERE Deliveries.weight BETWEEN ? AND ?
+                 """, (low_weight, high_weight))
+    else:
+        c.execute(query)
 
     data, columns = c.fetchall(), [description[0] for description in c.description]
 
@@ -91,53 +112,34 @@ def create_query_frame(root, query):
     tk.Button(root, text='Back to Queries', command=lambda: query_menu(root)).pack(pady=10)
 
 
-def query_1():
-    c.execute("SELECT * FROM Customers WHERE Suburb LIKE 'K%'")
-    return c.fetchall(), [description[0] for description in c.description]
-
-def query_2():
-    c.execute("SELECT delivery_docket, collected_from, date_collected, weight, deliver_to, date_delivered FROM Deliveries WHERE driver_id = (SELECT driver_id FROM Drivers WHERE first_name = 'Brad' AND last_name = 'Johnson')")
-    return c.fetchall(), [description[0] for description in c.description]
-
-def query_3():
-    c.execute("SELECT * FROM Deliveries WHERE deliver_to = 'Subiaco' AND weight < 5")
-    return c.fetchall(), [description[0] for description in c.description]
-
-def query_4():
-    c.execute("SELECT * FROM Customers WHERE post_code BETWEEN 6030 AND 6090 ORDER BY post_code DESC")
-    return c.fetchall(), [description[0] for description in c.description]
-
-def query_5():
-    c.execute("")
-    return c.fetchall(), [description[0] for description in c.description]
-
 def query_menu(root):
     for widget in root.winfo_children():
         widget.destroy()
 
     tk.Label(root, text="Query menu",font=("Arial", 16, "bold")).pack(pady=40)
 
-    
-    # todo: ask about inner joins, maybe do it for all deliveries (i.e include customers name and drivers name)
     queries = [
         ("List of customers' details who live in a suburb that start with a K", 
          """SELECT * FROM Customers 
             WHERE Suburb LIKE 'K%'"""),
         ("Brad Johnson's deliveries", 
-         """SELECT delivery_docket, collected_from, date_collected, weight, deliver_to, date_delivered, customer_id 
+         """SELECT delivery_docket, collected_from, date_collected, weight, deliver_to, date_delivered 
             FROM Deliveries 
             WHERE driver_id = (SELECT driver_id FROM Drivers WHERE first_name = 'Brad' AND last_name = 'Johnson')"""),
         ("Deliveries under 5kg going to Subiaco", 
-         """SELECT * FROM Deliveries 
-            WHERE deliver_to = 'Subiaco' AND weight < 5"""),
+         """SELECT Deliveries.delivery_docket, Deliveries.collected_from, Deliveries.date_collected, Deliveries.weight, Deliveries.date_delivered,
+            CONCAT(Drivers.first_name, ' ', Drivers.last_name) AS driver
+            FROM Deliveries
+            INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id
+            WHERE Deliveries.deliver_to = 'Subiaco' AND Deliveries.weight < 5"""),
         ("Customers in postcodes 6030-6090", 
          """SELECT * FROM Customers 
             WHERE post_code BETWEEN 6030 AND 6090 
             ORDER BY post_code DESC"""),
-        ("Specific Date", 
-         "query_5"), # todo
-        ("Weight Range", 
-         "query_6"), # todo
+        ("Delivery Details from a Specific Date", 
+         "query_5"),
+        ("Driver Details based on a range of package weights", 
+         "query_6"),
         ("Show heavy or light", 
          """SELECT delivery_docket, collected_from, date_collected, weight, deliver_to, date_delivered, 
             CASE 
@@ -146,7 +148,7 @@ def query_menu(root):
                 ELSE 'Invalid Weight' 
             END AS weight_category 
             FROM Deliveries"""),
-        ("Number of delivers and drivers", 
+        ("Number of deliveries and drivers", 
          """SELECT 
             (SELECT COUNT(*) FROM Deliveries) AS deliveries, 
             (SELECT COUNT(*) FROM Drivers) AS drivers"""),
@@ -157,14 +159,15 @@ def query_menu(root):
             MAX(Weight) AS Highest 
             FROM Deliveries"""),
         ("Fees", 
-         """SELECT Deliveries.delivery_docket, Deliveries.collected_from, Deliveries.date_collected, Deliveries.weight, Deliveries.deliver_to, Deliveries.date_delivered, 
-            Drivers.first_name AS driver_first_name, Drivers.last_name AS driver_last_name, 
-            (10+(10*weight)) AS Fee 
+         """SELECT Deliveries.delivery_docket, Deliveries.collected_from, Deliveries.date_collected, Deliveries.weight, (10+(10*weight)) AS Fee, Deliveries.deliver_to, Deliveries.date_delivered, 
+            CONCAT(Customers.first_name, ' ', Customers.last_name) AS customer,
+            CONCAT(Drivers.first_name, ' ', Drivers.last_name) AS driver
             FROM Deliveries 
-            INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id"""),
+            INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id
+            INNER JOIN Customers ON Deliveries.customer_id = Customers.customer_id"""),
         ("Mystery Package", 
          """SELECT Deliveries.delivery_docket, Deliveries.collected_from, Deliveries.date_collected, Deliveries.weight, Deliveries.deliver_to, Deliveries.date_delivered, 
-            CONCAT(Customers.first_name, ' ', Customers.last_name) AS Customer
+            CONCAT(Customers.first_name, ' ', Customers.last_name) AS customer
             FROM Deliveries
             INNER JOIN Customers ON Deliveries.customer_id = Customers.customer_id
             WHERE Deliveries.date_delivered > 12/01/2037 AND Deliveries.weight > 3 AND Customers.last_name LIKE 'A%'""")
@@ -174,6 +177,77 @@ def query_menu(root):
         tk.Button(root, text=label, command=lambda q=query: create_query_frame(root, q)).pack(pady=5)
 
     tk.Button(root, text="Back to home", command=lambda: show_intro_screen(root)).pack(pady=10)
+
+def export_to_csv(query):
+    c.execute(query)
+    results = c.fetchall()
+
+    if not results:
+        messagebox.showerror("No results", "No data to export")
+        return
+    
+    filepath = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+    if not filepath:
+        messagebox.showerror("No folder chosen", "Please select a valid filepath")
+        return
+    
+    try:
+        with open(filepath, 'w+', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([desc[0] for desc in c.description])
+            writer.writerows(results)
+        messagebox.showinfo("Success", f"Report exported to {filepath}")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+def create_report_frame(root, query):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    c.execute(query)
+    
+    data, columns = c.fetchall(), [desc[0] for desc in c.description]
+
+    tree = ttk.Treeview(root, columns=columns, show='headings')
+    for col in columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100)
+    for row in data:
+        tree.insert('', tk.END, values=row)
+
+    tree.pack(expand=True, fill='both', padx=20, pady=10)
+    tk.Button(root, text="Export to CSV", command=lambda q=query: export_to_csv(q)).pack(pady=10)
+    tk.Button(root, text='Back to Home', command=lambda: show_intro_screen(root)).pack(pady=10)
+
+def report_menu(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    queries = [
+        ("Deliveries from Belmont over 3.5kg",
+         """
+         SELECT Deliveries.delivery_docket AS "Delivery Docket", Deliveries.collected_from AS "Suburb Collected From", Deliveries.date_collected AS "Date Collected by Courier", Deliveries.weight AS "Weight of Package (KG)", Deliveries.deliver_to AS "Suburb Delivered To", Deliveries.date_delivered AS "Date Delivered",
+         CONCAT(Drivers.first_name, ' ', Drivers.last_name) AS Driver,
+         CONCAT(Customers.first_name, ' ', Customers.last_name) AS Customer
+         FROM Deliveries
+         INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id
+         INNER JOIN Customers ON Deliveries.customer_id = Customers.customer_id
+         WHERE Deliveries.collected_from = 'Belmont' AND Deliveries.weight > 3.5"""),
+         ("Driver details that have picked up packages from 10/01/37 to 12/01/37",
+          """
+          SELECT Deliveries.date_collected AS "Date Collected", 
+          Drivers.first_name AS "First Name", Drivers.last_name AS "Last Name", Drivers.street_address AS "Street Address", Drivers.Suburb AS "Suburb", Drivers.post_code AS "Post Code", Drivers.phone_number AS "Phone Number"
+          FROM Deliveries
+          INNER JOIN Drivers ON Deliveries.driver_id = Drivers.driver_id
+          WHERE Deliveries.date_collected BETWEEN '10/01/2037' AND '12/01/2037'""")
+    ]
+    tk.Label(root, text="Report Menu", font=("Arial", 16, "bold")).pack(pady=10)
+
+    for label, qurey in queries:
+        tk.Button(root, text=label, command=lambda q=qurey: create_report_frame(root, q)).pack(pady=10)
+
+    tk.Button(root, text="Back to Home", command=lambda: show_intro_screen(root)).pack(pady=10, padx=10)
+    
 
 def fetch_table_data(table_name):
     c.execute(f"SELECT * FROM {table_name}")
@@ -213,6 +287,7 @@ def show_intro_screen(root):
         tk.Button(root, text=f"View {label} Table", command=lambda t=table: create_table_frame(root, t)).pack(pady=10)
 
     tk.Button(root, text="Queries", command=lambda: query_menu(root)).pack(pady=10)
+    tk.Button(root, text="Reports", command=lambda: report_menu(root)).pack(pady=10)
 
 print("Setting up DB...")
 create_tables()
